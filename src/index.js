d4c483f9b51c54a643cc66be4163c0e753243f90
Samy Pessé <samypesse@gmail.com>
2014-07-27 19:01:49 -0700

    Package settings will now only contain an UI

--- src/index.js
+++ src/index.js
@@ -1,26 +1,3 @@
-define([
-    "src/manager"
-], function(Manager) {
-    var commands = codebox.require("core/commands");
+define(function(Manager) {
 
-    var manager = new Manager();
-
-    commands.register({
-        id: "settings.open",
-        title: "Settings: Open",
-        shortcuts: [
-            "mod+,"
-        ],
-        run: function(args, context) {
-            return commands.run("file.open", {
-                file: manager.getFile()
-            });
-        }
-    });
-
-    // Exports settings manager
-    codebox.settings = manager;
-
-    // Load settings
-    manager.load();
 });
