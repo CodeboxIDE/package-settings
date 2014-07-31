361fa45b54bacf54f5aee8277ff3edbfaa2e76c2
Samy Pessé <samypesse@gmail.com>
2014-07-31 16:18:35 -0700

    Add base for settings UI tab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-define(function(Manager) {
+define([
+    "src/tab"
+], function(Tab) {
+    var commands = codebox.require("core/commands");
 
+    // Modify current command to open settings
+    var openSettings = commands.get("settings.open");
+    var baseRunner = openSettings.get("run");
+
+    openSettings.set("run", function() {
+        return codebox.tabs.add(Tab, {}, {
+            uniqueId: "settings",
+            type: "settings",
+            title: "Settings"
+        });
+    });
 });
--- src/tab.js
+++ src/tab.js
@@ -1,14 +1,17 @@
-define([
-    "text!src/templates/tab.html"
-], function(templateFile) {
+define(function() {
     var hr = codebox.require("hr/hr");
+    var FormView = codebox.require("views/form");
 
     var Tab = codebox.tabs.Panel.extend({
         className: "component-settings",
-        template: templateFile,
 
         initialize: function() {
             Tab.__super__.initialize.apply(this, arguments);
+
+            this.form = new FormView({
+                schema: codebox.settings.toSchema(),
+                values: codebox.settings.exportJson()
+            })
         },
 
         templateContext: function() {
